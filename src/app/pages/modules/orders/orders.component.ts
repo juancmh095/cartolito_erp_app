b56ab4ad9fb97d9fa8ce8d1a3440b5a3eeaa4467
juancmh095 <juancarlosmh095@gmail.com>
2024-09-18 13:11:06 -0600

    x

--- src/app/pages/modules/orders/orders.component.ts
+++ src/app/pages/modules/orders/orders.component.ts
@@ -1,20 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OpdfComponent } from './opdf/opdf.component';
 import { UtilsService } from '../../../../services/utils.service';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../../../services/api.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
   imports: [
-    OpdfComponent
+    OpdfComponent,
+    FormsModule,
+    NgFor,
+    CommonModule
   ],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
 
-  constructor(private _utils:UtilsService){}
+  formData: any = {};
+  catalogos: any = {};
+  productos:any = [];
+  maquinas:any = [];
+  proveedores:any = [];
+  useReq:any = false;
 
+
+  constructor(
+    private _utils:UtilsService,
+    private _api:ApiService
+  ){}
+
+
+  ngOnInit(){
+    this.getCatalogue()
+    this.getDataList();
+  }
+
+  async getDataList(){
+    var responseProv = await this._api.get('catalogos/proveedores',{});
+    this.proveedores = responseProv.data;
+    var responseMaq = await this._api.get('catalogos/maquinas',{});
+    this.maquinas = responseMaq.data;
+    var responseProd = await this._api.get('catalogos/productos',{});
+    this.productos = responseProd.data;
+    console.log(this.productos,this.maquinas,this.proveedores)
+  }
+
+  async getCatalogue(){
+    var response = await this._api.get('catalogos',{});
+    var data = response.data;
+    this.catalogos.cc = data.filter((item:any) => item.name === 'CENTRO_COSTOS')[0]['items']
+    this.catalogos.suc = data.filter((item:any) => item.name === 'SUCURSALES')[0]['items']
+    this.catalogos.um = data.filter((item:any) => item.name === 'Unidad_Medida')[0]['items']
+    this.catalogos.um = data.filter((item:any) => item.name === 'Unidad_Medida')[0]['items']
+    console.log(this.catalogos);
+  }
+
+  onSubmit(form: any) {
+    console.log(this.formData)
+    if (form.valid) {
+      console.log('Formulario enviado:', this.formData);
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
   print(){
     this._utils.print('printAreaOPDF');
   }
