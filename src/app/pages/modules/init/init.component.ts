218b939c5ddcafa148276aeeac136d5c83b5c48a
Juan Carlos Méndez Hernández <41524371+juancmh095@users.noreply.github.com>
2024-11-13 05:34:01 -0600

    camnbios en el dashboard y entrega material

--- src/app/pages/modules/init/init.component.ts
+++ src/app/pages/modules/init/init.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../../services/api.service';
 
 @Component({
   selector: 'app-init',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './init.component.html',
   styleUrl: './init.component.css'
 })
-export class InitComponent {
+export class InitComponent implements OnInit {
 
+  indicadores:any = {};
+
+  constructor(private _api:ApiService) { }
+
+  async ngOnInit() {
+    var response:any = await this._api.get('requisicion/indicadores', {});
+    this.indicadores = response.data;
+    console.log(response)
+  }
 }
--- src/app/pages/modules/materials/inputs-material/inputs-material.component.ts
+++ src/app/pages/modules/materials/inputs-material/inputs-material.component.ts
@@ -1,12 +1,15 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { ApiService } from '../../../../../services/api.service';
 import { FormsModule, NgModel } from '@angular/forms';
+import { CurrencyPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-inputs-material',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgFor,
+    CurrencyPipe
   ],
   templateUrl: './inputs-material.component.html',
   styleUrl: './inputs-material.component.css'
@@ -15,7 +18,7 @@ export class InputsMaterialComponent implements OnInit{
 
   data:any = {};
   isReq:any = false;
-  folio:any = 0;
+  folio:any = '';
 
   constructor(private _api:ApiService){}
 
@@ -27,9 +30,15 @@ export class InputsMaterialComponent implements OnInit{
     if(this.isReq){
       let response = await this._api.get('requisicion',{folio:this.folio});
       console.log('req',response)
+      this.data = response.data[0];
     }else{
       let response = await this._api.get('oc',{folio:this.folio});
+      this.data = response.data[0];
       console.log('oc',response)
     }
+    this.data.items = this.data.items.map((item:any)=>{
+      item.rec = 0
+      return item;
+    })
   }
 }
